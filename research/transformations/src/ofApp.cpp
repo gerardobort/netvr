728c7f765ffc2388ea60f5f19e88d721c26b09fd
Gerardo Bort <gerardo.bort@rga.com>
2014-11-19 16:42:28 -0300

    successful attempt to get inverse matrix working as antitransformation technique

---
+++ research/transformations/src/ofApp.cpp
@@ -0,0 +1,83 @@
+#include "ofApp.h"
+#include <math.h>
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+    ofSetLogLevel(OF_LOG_VERBOSE);
+    ofSetFrameRate(60);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+    image.loadImage("bort.jpg");
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+    ofColor centerColor(100, 100, 100);
+    ofColor edgeColor(40, 40, 40);
+    ofBackgroundGradient(centerColor, edgeColor, OF_GRADIENT_CIRCULAR);
+    ofEnableDepthTest();
+	ofSetColor(255, 255, 255);
+
+    int w = image.width;
+    int h = image.height;
+
+    ofPixels pixels1 = image.getPixelsRef();
+    unsigned char pixels2[w*h*3];
+    ofTexture out1;
+    out1.allocate(w, h, GL_RGB);
+    ofMesh mesh1;
+    ofMesh mesh2;
+
+    for (int u = 0; u < w; u++)
+        for (int v = 0; v < h; v++) {
+            int i = 3*(v*w + u);
+            ofColor color = ofColor(pixels1[i +0], pixels1[i +1], pixels1[i +2], 255);
+            ofVec3f vertex = ofVec3f(u, v, 0);
+
+            ofMatrix4x4 m = ofMatrix4x4::newTranslationMatrix(0.001, 0.001, 0) * ofMatrix4x4::newRotationMatrix(45, 1.0, 1.0, 1.0) * ofMatrix4x4::newScaleMatrix(0.5, 0.5, 0.5);
+            vertex = m.postMult(vertex);
+            mesh1.addColor(color);
+            mesh1.addVertex(vertex);
+
+            vertex = m.getInverse().postMult(vertex);
+            mesh2.addColor(color);
+            mesh2.addVertex(vertex);
+        }
+
+
+    ofFbo fbo1;
+    fbo1.allocate(w, h);
+    fbo1.begin();
+        ofBackground(ofColor(0, 0, 0, 0));
+        image.draw(0, 0);
+    fbo1.end();
+    fbo1.draw(0, 0, w, h);
+
+    ofFbo fbo2;
+    fbo2.allocate(w, h);
+    fbo2.begin();
+        ofBackground(ofColor(0, 0, 0, 0));
+        mesh1.drawVertices();
+    fbo2.end();
+    fbo2.draw(w, 0, w, h);
+
+    ofFbo fbo3;
+    fbo3.allocate(w, h);
+    fbo3.begin();
+        ofBackground(ofColor(0, 0, 0, 0));
+        mesh2.drawVertices();
+    fbo3.end();
+    fbo3.draw(0, h, w, h);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::exit(){
+}
---
+++ research/transformations/src/ofApp.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofGLProgrammableRenderer.h"
+
+#define M_PI 3.1415926535897932384626433832795
+
+class ofApp : public ofBaseApp{
+
+    public:
+        void setup();
+        void update();
+        void draw();
+        void exit();
+    
+        ofImage image;
+};
