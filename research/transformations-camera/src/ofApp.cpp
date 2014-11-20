39e00b4eb7c5177b306999c28ec9b919505a7e60
Gerardo Bort <gerardo.bort@rga.com>
2014-11-20 10:05:27 -0300

    code refactor..

--- research/transformations-camera/src/ofApp.cpp
+++ research/transformations-camera/src/ofApp.cpp
@@ -29,8 +29,8 @@ void ofApp::draw(){
     ofEnableDepthTest();
 	ofSetColor(255, 255, 255);
 
-    int w = image.width;
-    int h = image.height;
+    int w = image.width/2.0;
+    int h = image.height/2.0;
 
     ofPixels pixels1 = image.getPixelsRef();
     unsigned char pixels2[w*h*3];
@@ -56,51 +56,51 @@ void ofApp::draw(){
         }
 
 
+    stringstream reportStream;
+
     ofFbo fbo1;
     fbo1.allocate(w, h);
     fbo1.begin();
         ofBackground(ofColor(0, 0, 0, 0));
         image.draw(0, 0);
+
+        reportStream.str(""); reportStream.clear();
+        reportStream << "original" << endl;
+	    ofDrawBitmapString(reportStream.str(), 20, 20);
     fbo1.end();
     fbo1.draw(0, 0, w, h);
 
-    /*
     ofFbo fbo2;
     fbo2.allocate(w, h);
     fbo2.begin();
         ofBackground(ofColor(0, 0, 0, 0));
-        mesh1.drawVertices();
+        camera.begin();
+        image.draw(0, 0);
+        camera.end();
+
+        reportStream.str(""); reportStream.clear();
+        reportStream << "camera view transformation" << endl;
+	    ofDrawBitmapString(reportStream.str(), 20, 20);
     fbo2.end();
     fbo2.draw(w, 0, w, h);
 
-    ofFbo fbo3;
-    fbo3.allocate(w, h);
-    fbo3.begin();
-        ofBackground(ofColor(0, 0, 0, 0));
-        mesh2.drawVertices();
-    fbo3.end();
-    fbo3.draw(0, h, w, h);
-    */
 
     ofFbo fbo3;
     fbo3.allocate(w, h);
     fbo3.begin();
         ofBackground(ofColor(0, 0, 0, 0));
         ofMatrix4x4 m = camera.getModelViewMatrix();
+        ofPushMatrix();
         ofLoadMatrix(m);
         image.draw(0, 0);
+        ofPopMatrix();
+
+        reportStream.str(""); reportStream.clear();
+        reportStream << "custom transformation" << endl;
+	    ofDrawBitmapString(reportStream.str(), 20, 20);
     fbo3.end();
     fbo3.draw(0, h, w, h);
 
-    ofFbo fbo4;
-    fbo4.allocate(w, h);
-    fbo4.begin();
-        ofBackground(ofColor(0, 0, 0, 0));
-        camera.begin();
-        image.draw(0, 0);
-        camera.end();
-    fbo4.end();
-    fbo4.draw(w, h, w, h);
 
 }
 
